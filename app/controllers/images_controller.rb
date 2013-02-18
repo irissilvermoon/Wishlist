1b0457aa2d0724665f1ac90d90a9b7330c8e5922
Karen Caplan <keaslk@gmail.com>
2013-02-17 20:35:41 -0800

    implemented fakeweb responses, stubbed out requests to flickr.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,9 @@
 class ImagesController < ApplicationController
 
   def new
-    
+    @wish_list = WishList.find(params[:wish_list_id])
+    @item = @wish_list.items.find(params[:item_id])
+    page = MetaInspector.new(@item.link)
+    puts page.document
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'fakeweb'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -35,4 +36,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  FakeWeb.allow_net_connect = false
+  FakeWeb.register_uri(:get, "http://www.flickr.com/photos/irissilvermoon", :body => File.read(Rails.root.join("spec/support/fakeweb_responses/iris_flickr.html")))
 end
