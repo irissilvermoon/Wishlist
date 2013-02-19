567df57f05f5daac82f921222dc500570b1dd248
Karen Caplan <keaslk@gmail.com>
2013-02-18 20:29:44 -0800

    url maps to array of images at items/:id/image/new to be chosen from.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,7 +3,7 @@ class ImagesController < ApplicationController
   def new
     @wish_list = WishList.find(params[:wish_list_id])
     @item = @wish_list.items.find(params[:item_id])
-    page = MetaInspector.new(@item.link)
-    puts page.document
+    @page = MetaInspector.new(@item.link)
+    @images = @page.images
   end
 end
