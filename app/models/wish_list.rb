f95611822504972a38313cb4f992c54aa87e2133
Karen Caplan <keaslk@gmail.com>
2013-02-25 21:28:06 -0800

    added css around images for choosing images, buttons for new items/wish lists

--- app/models/wish_list.rb
+++ app/models/wish_list.rb
@@ -2,5 +2,5 @@ class WishList < ActiveRecord::Base
   attr_accessible :description, :title
   validates :title, :presence => true
 
-  has_many :items
+  has_many :items, :dependent => :destroy
 end
