0e4c57305d098cc5e8cefe00259a93a392d28edc
Karen Caplan <keaslk@gmail.com>
2013-03-22 11:03:05 -0700

    cleaning up git repo with deleted files, added ability for users to invite others

---
+++ spec/integration/inviting_users_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+feature 'inviting users' do
+  let!(:user) { Factory(:confirmed_user) }
+
+  before do
+    sign_in_as!(user)
+    visit '/'
+    click_link 'Invite User'
+  end
+
+  scenario 'User can invite another user' do
+    fill_in "Email", :with => "newuser@wishlist.com"
+    click_button "Send an invitation"
+    page.should have_content("An invitation email has been sent")
+  end
+
+  scenario "Users can sign up via invite" do
+    fill_in "Email", :with => "newuser@wishlist.com"
+    click_button "Send an invitation"
+    click_link "Sign out"
+
+    open_email "newuser@wishlist.com", :with_subject => "You've been invited to Wishlist!"
+    User.first.confirmation_token
+    current_email.body
+    click_email_link_matching(/invitation/)
+
+    page.should have_content("Set your password")
+    fill_in "Password", :with => "password"
+    fill_in "Password confirmation", :with => "password"
+    click_button "Set my password"
+    page.should have_content("Welcome to Wishlist! You are now signed in!")
+  end
+end
