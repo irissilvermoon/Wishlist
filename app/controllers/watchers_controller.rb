1004754e184878ba6ec522a42cb6f23de06bae1b
Karen Caplan <keaslk@gmail.com>
2013-03-25 22:48:26 -0700

    users can be created and added to list of watchers on a user's wishlist

--- app/controllers/watchers_controller.rb
+++ app/controllers/watchers_controller.rb
@@ -3,4 +3,10 @@ class WatchersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.invite!(params[:user], current_user)
+    current_user.watchers << @user
+    head :ok
+  end
 end
--- spec/controllers/watchers_controller_spec.rb
+++ spec/controllers/watchers_controller_spec.rb
@@ -4,11 +4,29 @@ describe WatchersController do
   let(:user) { Factory(:confirmed_user) }
   let(:wish_list) { Factory(:wish_list) }
 
+  before do
+    sign_in(:user, user)
+  end
+
   describe "#new" do
     it "successfully renders new" do
-      sign_in(:user, user)
       get :new
       response.should render_template("new")
+    end
+  end
+
+  describe "#create" do
+    context "with a user that doesn't exist" do
+      it "should create a user in the system" do
+        post :create, :user => {:email => "example@example.com"}
+        User.where(:email => "example@example.com").should be_present
+      end
+
+      it "should add the user as a watcher" do
+        post :create, :user => {:email => "example@example.com"}
+        new_user = User.where(:email => "example@example.com").first
+        new_user.should be_in user.watchers
+      end
     end
   end
 end
