1c368b0ace32559a95674975d4c59ef6ecb9ee9d
Karen Caplan <keaslk@gmail.com>
2013-03-27 20:57:44 -0700

    trying to check if user present in watchers list

--- app/controllers/watchers_controller.rb
+++ app/controllers/watchers_controller.rb
@@ -7,12 +7,20 @@ class WatchersController < ApplicationController
   end
 
   def create
-    @user = User.invite!(params[:user], current_user)
-    current_user.watchers << @user
-    redirect_to watchers_path, notice: "#{@user.email} was added as a watcher."
+    #check if user is present in current_user.watchers list
+    #if not, add them.
+    @user = current_user.watchers.all
+    if @user.present?
+      redirect_to watchers_path, notice: "#{@user.email} is already a watcher."
+    else
+      @user = User.invite!(params[:user], current_user)
+      current_user.watchers << @user
+      redirect_to watchers_path, notice: "#{@user.email} was added as a watcher."
+    end
   end
 
   def index
     @watchers = current_user.watchers.all
   end
 end
+
--- spec/integration/inviting_users_to_wish_lists.rb
+++ spec/integration/inviting_users_to_wish_lists.rb
@@ -8,3 +8,19 @@ feature "Inviting users to Wishlists" do
     visit '/'
     click_link 'Invite User'
   end
+
+  scenario "Users can invite others to watch their Wishlists" do
+    fill_in "Email", :with => "person@wishlist.com"
+    click_button "Invite"
+    page.should have_content("person@wishlist.com was added as a watcher.")
+  end
+
+  scenario "User cannot add user who is already a watcher" do
+    fill_in "Email", :with => "person@wishlist.com"
+    click_button "Invite"
+    page.should have_content("person@wishlist.com was added as a watcher.")
+    click_link "Invite User"
+    fill_in "Email", :with => "person@wishlist.com"
+    page.should have_content("person@wishlist.com is already a watcher.")
+  end
+end
