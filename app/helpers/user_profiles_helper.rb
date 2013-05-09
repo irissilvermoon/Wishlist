3ee24e4bf18574df99b9c537f0e1f577429bddb5
Karen Caplan <keaslk@gmail.com>
2013-05-08 19:36:31 -0700

    more style changes to user profiles, added helper method for formatting addresses.

--- app/helpers/user_profiles_helper.rb
+++ app/helpers/user_profiles_helper.rb
@@ -1,2 +1,12 @@
 module UserProfilesHelper
+  def present_user_profile(profile)
+    out = ""
+    out << "#{profile.name}" if profile.name.present?
+    out << "\n#{profile.address1}" if profile.address1.present?
+    out << "\n#{profile.address2}" if profile.address2.present?
+    out << "\n#{profile.city}, #{profile.state} #{profile.ZIP}"
+    out << "\n#{profile.country}"
+
+    out.gsub("\n", "<br>").html_safe
+  end
 end
