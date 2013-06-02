3e375e9dcc69f51aa7162604024312449d293c49
Karen Caplan <keaslk@gmail.com>
2013-06-01 17:17:52 -0700

    passing delete tests for users to be able to undo purchased Items.

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -12,4 +12,12 @@ class PurchasesController < ApplicationController
   #make destroy method for undo
   #set purchased_by to nil
   #change route to :only => [:create, :destroy]
+
+  def destroy
+    @wish_list = WishList.find(params[:wish_list_id])
+    @item = @wish_list.items.find(params[:item_id])
+    @item.purchased_by = nil
+    @item.save
+    head 200
+  end
 end
--- spec/controllers/purchases_controller_spec.rb
+++ spec/controllers/purchases_controller_spec.rb
@@ -1,32 +1,44 @@
 require 'spec_helper'
 
 describe PurchasesController do
+  let!(:user) { Factory(:confirmed_user) }
+
+  let!(:watching_user) do
+    u = Factory(:confirmed_user)
+    user.watchers << u
+    u
+  end
+
+  let!(:wish_list) { Factory(:wish_list,
+                             :title => "new title",
+                             :description => "description of title!",
+                             :user => user) }
+
+  let!(:item) { Factory(:item,
+                        :title => "A Pony",
+                        :wish_list => wish_list,
+                        :link => "http://www.flickr.com/photos/irissilvermoon") }
+
+  before do
+    sign_in(:user, watching_user)
+  end
+
   describe '#create' do
-    let!(:user) { Factory(:confirmed_user) }
-
-    let!(:watching_user) do
-      u = Factory(:confirmed_user)
-      user.watchers << u
-      u
-    end
-
-    let!(:wish_list) { Factory(:wish_list,
-                               :title => "new title",
-                               :description => "description of title!",
-                               :user => user) }
-
-    let!(:item) { Factory(:item,
-                          :title => "A Pony",
-                          :wish_list => wish_list,
-                          :link => "http://www.flickr.com/photos/irissilvermoon") }
-
-    before do
-      sign_in(:user, watching_user)
-    end
-
     it 'should add a purchased to item' do
       post :create, :wish_list_id => wish_list.id, :item_id => item.id
       item.reload.purchased_by.should == watching_user
+    end
+  end
+
+  describe '#destroy' do
+    before do
+      item.purchased_by = watching_user
+      item.save
+    end
+
+    it 'should allow users to undo item purchases' do
+      delete :destroy, :wish_list_id => wish_list.id, :item_id => item.id
+      item.reload.purchased_by.should == nil
     end
   end
 end
